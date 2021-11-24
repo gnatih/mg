a93db4124cfad3313a56797ad65f77d095117bea
Jayesh Gohel <jayesh.gohel@gmail.com>
2021-11-24 21:30:39 +0530

    Add check for page for cart empty

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -8,7 +8,7 @@ do_action('woocommerce_cart_is_empty');
 
 $page = get_page_by_title('All Maps');
 
-if (wc_get_page_id('shop') > 0) { ?>
+if (wc_get_page_id('shop') > 0 && $page) { ?>
 <p class="return-to-shop">
 	<a class="button wc-backward" href="<?php echo get_post_permalink($page->ID) ?>">View all Maps</a>
 </p>
