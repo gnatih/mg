eb4077034aaf8dc3a05cccd7bdd119e892d8b9fa
Jayesh Gohel <jayesh.gohel@gmail.com>
2021-11-24 20:55:26 +0530

    Update empty-cart template

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -8,7 +8,6 @@ do_action('woocommerce_cart_is_empty');
 
 $page = get_page_by_title('All Maps');
 
-
 if (wc_get_page_id('shop') > 0) { ?>
 <p class="return-to-shop">
 	<a class="button wc-backward" href="<?php echo get_post_permalink($page->ID) ?>">View all Maps</a>
